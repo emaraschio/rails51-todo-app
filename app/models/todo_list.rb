45515c4eea379199ec4f065efeddd38053090218
Ezequiel Maraschio <ezequiel.maraschio@gmail.com>
2017-09-18 10:11:50 -0300

    Cleanup

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,13 +1,13 @@
 class TodoList < ApplicationRecord
-	validates :title, presence: true, length: { minimum: 4 }
-	validates :description, presence: true, length: { minimum: 4 }
-	has_many :todo_items, dependent: :destroy
+  validates :title, presence: true, length: { minimum: 4 }
+  validates :description, presence: true, length: { minimum: 4 }
+  has_many :todo_items, dependent: :destroy
 
-	def has_complete_items?
-		todo_items.complete.size > 0
-	end
+  def has_complete_items?
+    todo_items.complete.size > 0
+  end
 
-	def has_incomplete_items?
-		todo_items.incomplete.size > 0
-	end
+  def has_incomplete_items?
+    todo_items.incomplete.size > 0
+  end
 end
--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -2,33 +2,33 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe TodoList, type: :model do
-	it { should have_many(:todo_items) }
+  it { should have_many(:todo_items) }
 
-	describe "#has_complete_items?" do
-		let(:todo_list) { TodoList.create(title: "Food", description: "List of food to buy") }
+  describe "#has_complete_items?" do
+    let(:todo_list) { TodoList.create(title: "Food", description: "List of food to buy") }
 
-		it "returns true with completed todo list items" do
-			todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
-			expect(todo_list.has_complete_items?).to be true
-		end
+    it "returns true with completed todo list items" do
+      todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
+      expect(todo_list.has_complete_items?).to be true
+    end
 
-		it "returns false with no completed todo list items" do
-			todo_list.todo_items.create(content: "Eggs")
-			expect(todo_list.has_complete_items?).to be false
-		end
-	end
+    it "returns false with no completed todo list items" do
+      todo_list.todo_items.create(content: "Eggs")
+      expect(todo_list.has_complete_items?).to be false
+    end
+  end
 
-	describe "#has_incomplete_items?" do
-		let(:todo_list) { TodoList.create(title: "Food", description: "List of food to buy") }
+  describe "#has_incomplete_items?" do
+    let(:todo_list) { TodoList.create(title: "Food", description: "List of food to buy") }
 
-		it "returns true with incompleted todo list items" do
-			todo_list.todo_items.create(content: "Eggs")
-			expect(todo_list.has_incomplete_items?).to be true
-		end
+    it "returns true with incompleted todo list items" do
+      todo_list.todo_items.create(content: "Eggs")
+      expect(todo_list.has_incomplete_items?).to be true
+    end
 
-		it "returns false with no incompleted todo list items" do
-			todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
-			expect(todo_list.has_incomplete_items?).to be false
-		end
-	end
+    it "returns false with no incompleted todo list items" do
+      todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
+      expect(todo_list.has_incomplete_items?).to be false
+    end
+  end
 end
